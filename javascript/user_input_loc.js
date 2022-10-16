6d770c2a55d3d4e4681b57a6560ea2f4d9e48b08
murphylee10 <murphylee2004@gmail.com>
2022-10-16 09:07:40 -0400

    kdjfd

--- javascript/user_input_loc.js
+++ javascript/user_input_loc.js
@@ -7,9 +7,9 @@ var form = document.getElementById("form1");
 form.addEventListener('submit', function (event) {
     event.preventDefault()
 
-    latitude = document.getElementById('latitude').value
-    longitude = document.getElementById('longitude').value
-    radius = document.getElementById('radius').value
+    latitude = parseInt(document.getElementById('latitude').value)
+    longitude = parseInt(document.getElementById('longitude').value)
+    radius = parseInt(document.getElementById('radius').value)
 
     location.href = "initiative_list_page.html"
 
