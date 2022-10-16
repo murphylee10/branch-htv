1738a736ca9c9af4a1fff38b201b14fb31ebe91b
murphylee10 <murphylee2004@gmail.com>
2022-10-16 08:37:04 -0400

    updates

---
+++ javascript/user_input_loc.js
@@ -0,0 +1,12 @@
+var address;
+var radius;
+
+var form = document.getElementById("form1");
+
+form.addEventListener('submit', function (event) {
+    event.preventDefault()
+
+    address = document.getElementById('address').value
+    radius = document.getElementById('radius').value
+
+})
